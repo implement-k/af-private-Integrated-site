59ff7c74f2a2737ab5a320478a6a61140de3bce2
koohyun kwon <rnjsrn98@gmail.com>
2023-11-20 12:21:17 +0000

    why

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,url_for,request
+from connect_afpsDB import con
 
 app = Flask(__name__)
 
@@ -18,7 +19,19 @@ def login():
 @app.route('/signup/',methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
-        return render_template('signup_done.html')
+        # try:
+            uid = request.form.get('id')
+            upw = request.form.get('pw_dub')
+            upn = request.form.get('num')
+            upn.replace('-', '')
+            cur = con.cursor()
+            cur.execute("INSERT INTO user(u_id,u_pw,u_phoneNum) VALUES ('{0}','{1}','{2}');".format(uid,upw,upn))
+            con.commit()
+            con.close()
+            return render_template('signup_done.html')
+        # except:
+        #     return 'error'
+        
     else:
         return render_template('signup.html')
 
