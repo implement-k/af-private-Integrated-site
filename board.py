2a18dbc795512460b8120e86b68d2ad65e2c6e6f
koohyun kwon <rnjsrn98@gmail.com>
2023-11-30 14:54:56 +0000

    최적화

--- board.py
+++ board.py
@@ -1,19 +1,10 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from connect_afpsDB import U,P,H,D
-import pymysql
-import uuid
-import re
+import pymysql, uuid, re
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
 
-post_list = [
-    {'id':'1', 'favorite':0, 'user':'권구현', 'title':'제목제목asdfasdfasdf', 'content':'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇ', 'created':'11'},
-    {'id':'2', 'favorite':100, 'user':'일구현', 'title':'제목제asdfasdf목', 'content':'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇㅁㄴㅇㄹㅁㄴㅇㄹ','created':'11'},
-    {'id':'3', 'favorite':200, 'user':'이구현', 'title':'제목', 'content':'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇ','created':'11'},
-    {'id':'4', 'favorite':39, 'user':'삼구현', 'title':'목', 'content':'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻ','created':'11'},
-    {'id':'5', 'favorite':2, 'user':'사구현', 'title':'제목제목', 'content':'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄹㄴㅇ','created':'11'}
-]
 
 def db(isOutput, sql):
     con = pymysql.connect(user=U,passwd=P,host=H,db=D,charset='utf8')
@@ -35,66 +26,113 @@ def db(isOutput, sql):
 @app.route('/login/',methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        uid = request.form.get('id')
-        upw = request.form.get('password')
-        if len(uid) == 0 or len(upw) == 0:
-            return redirect('/login')
-        t = len(db(True, ["SELECT u_id FROM user WHERE u_id = '{0}' AND u_pw = '{1}';".format(uid, upw)])[0])
-        if t > 0:
-            session['id'] = uid
-            cl = db(True, ["SELECT u_clike1,u_clike2,u_clike3 FROM user WHERE u_id = '{0}'".format(uid)])
-            n = cl[0][0]
-            for i in range(3):
-                session[str(i+1)] = n[i]
-                    
+        con = pymysql.connect(user=U,passwd=P,host=H,db=D,charset='utf8')
+        cur = con.cursor()
+        user_id = request.form.get('id')
+        user_pw = request.form.get('password')
+
+        if len(user_id) == 0 or len(user_pw) == 0: return redirect('/login')
+        
+        cur.execute("SELECT u_id FROM user WHERE u_id = '{0}' AND u_pw = '{1}';".format(user_id, user_pw))
+        isUser = len(cur.fetchall())
+
+        if isUser > 0:
+            session['id'] = user_id
+
+            cur.execute("SELECT u_clike1,u_clike2,u_clike3 FROM user WHERE u_id = '{0}'".format(user_id))
+            classLike_list = cur.fetchall()[0]
+
+            for i in range(3): session[str(i+1)] = classLike_list[i]
+
+            con.close()
             return redirect('/')
         else:
+            con.close()
             return render_template('login.html')
     else:
-        if "id" in session:
-            return redirect('/')
-        else:
-            return render_template('login.html')
+        if "id" in session: return redirect('/')
+        else: return render_template('login.html')
+
 
 @app.route('/logout/')
 def logout():
-    if session.get('id', None):
+    if "id" in session:
         session.pop('id',None)
-        for i in range(3):
-            s = 'c_like' + str(i+1)
-            session.pop(s, None)
+
+        for i in range(3): session.pop(str(i+1), None)
+
     return redirect('/')
+
 
 @app.route('/signup/',methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
         try:
-            uid = request.form.get('id')
-            upw = request.form.get('pw_dub')
-            upn = request.form.get('num').replace("-", "")
-            vid = '^[A-Za-z0-9._]{1,20}$'
-            vpw = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,25}$'
-            if not re.fullmatch(vid, uid) or not re.fullmatch(vpw, upw):
-                return '잘못된 접근'
-            db(False, ["INSERT INTO user(u_id,u_pw,u_phoneNum) VALUES ('{0}','{1}','{2}');".format(uid,upw,upn)])
+            user_id = request.form.get('id')
+            user_pw = request.form.get('pw_dub')
+            user_phoneNumber = request.form.get('num').replace("-", "")
+            valid_id = '^[A-Za-z0-9._]{1,20}$'
+            valid_pw = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,25}$'
+
+            if not re.fullmatch(valid_id, user_id) or not re.fullmatch(valid_pw, user_pw): return '잘못된 접근'
+
+            db(False, ["INSERT INTO user(u_id,u_pw,u_phoneNum) VALUES ('{0}','{1}','{2}');".format(user_id, user_pw, user_phoneNumber)])
             return render_template('signup_done.html')
         except: return 'error'
     else:
-        if "id" in session:
-            return redirect('/')
-        else:
-            return render_template('signup.html')
+        if "id" in session: return redirect('/')
+        else: return render_template('signup.html')
+        
 
-@app.route('/isDub',methods=['POST'])
-def dubCheck():
-    try:
-        name = request.form['name']
-        value = request.form['value']
-        return str(len(db(True, ["SELECT {0} FROM user WHERE {0}='{1}'".format(name, value)]))[0]) #중복이면 1, 중복 아니면 0
-    except Exception as e:
-        return "error"
+@app.route('/board/<int:isShow>&<int:class_id>&<class_name>',methods=['POST', 'GET'])
+def board(isShow, class_id, class_name):    #isShow 1: 글보기, isShow 0: 글작성
+    user_id = session.get('id', None)
+    class_info = [class_id, class_name]     #index 0: class id, index 1: class name
 
-@app.route('/LikeClass/<int:isAdd>',methods=['POST'])
+    if isShow == 1:
+        isLiked = [0 for i in range(100)]
+
+        if not user_id and class_id != 1: return redirect('/login')     #로그인 안하거나 익명도 글 작성이 가능한 게시판이 아니라면 로그인페이지로 리다이렉트
+
+        post_list = db(True, ["SELECT * FROM posts WHERE p_classification = {0}".format(class_id)])[0]
+        
+        #TODO start 좋아요 눌렀는지 확인 => 
+        isLike = 0
+        for i in range(3):
+            cid = session.get(str(i+1), None)
+            if cid != 0 and cid != None: isLiked[cid-1] = 1
+
+        if isLiked[class_id-1] == 1:
+            isLike = 1
+        #TODO end 좋아요 눌렀는지 확인 => 
+
+        return render_template('board.html', post_list=post_list, uid=user_id, class_info=class_info, isLiked=isLike)
+    elif isShow == 0:
+        if not user_id and class_id != 1: return redirect('/login')
+        else: return render_template('write.html', class_info = class_info)
+
+
+@app.route('/',methods=['POST', 'GET'])
+def home():
+    if "id" in session:
+        liked_class_list = []
+                
+        class_list = db(True, ["SELECT * FROM post_class"])[0]
+
+        for i in range(3):
+            class_id = session.get(str(i+1), None)
+            for c in class_list:
+                if class_id == c[0]:
+                    liked_class_list.append([class_id, c[1]])
+                    break
+            
+        return render_template('home.html', class_list=class_list, liked_class_list=liked_class_list)
+    else:
+        return redirect('/board/1&1&계룡대')
+
+
+#TODO 좋아요 누르는 쿼리 다시 짜기
+@app.route('/likeClass/<int:isAdd>',methods=['POST'])
 def likeClass(isAdd):
     if isAdd == 1:
         cid = request.form['classID']
@@ -127,59 +165,16 @@ def likeClass(isAdd):
             return 's'
         except:
             return redirect('/board/'+str(cid))
-        
-
-@app.route('/board/<int:isShow>&<int:class_id>&<class_name>',methods=['POST', 'GET'])
-def board(isShow, class_id, class_name):        #isShow 1: 글보기, isShow 0: 글작성
-    user_id = session.get('id', None)
-    class_info = [class_id, class_name]     #index 0: class id, index 1: class name
-    if isShow == 1:
-        isLiked = [0 for i in range(100)]
-
-        if not user_id and class_id != 1: return redirect('/login')     #로그인 안하거나 익명도 글 작성이 가능한 게시판이 아니라면 로그인페이지로 리다이렉트
-
-        post_list = db(True, ["SELECT * FROM posts WHERE p_classification = {0}".format(class_id)])[0]
-        
-        #좋아요 눌렀는지 확인 => 캐시로 해도 될 것 같음
-        isLike = 0
-        for i in range(3):
-            cid = session.get(str(i+1), None)
-            if cid != 0 and cid != None: isLiked[cid-1] = 1
-
-        if isLiked[class_id-1] == 1:
-            isLike = 1
-
-        return render_template('board.html', post_list=post_list, uid=user_id, class_info=class_info, isLiked=isLike)
-    elif isShow == 0:
-        if not user_id and class_id != 1: return redirect('/login')
-        else: return render_template('write.html', class_info = class_info)
 
 
+@app.route('/isDub',methods=['POST'])
+def dubCheck():
+    try:
+        name = request.form['name']
+        value = request.form['value']
+        return str(len(db(True, ["SELECT {0} FROM user WHERE {0}='{1}'".format(name, value)]))[0]) #중복이면 1, 중복 아니면 0
+    except Exception as e:
+        return "error"
 
-@app.route('/',methods=['POST', 'GET'])
-def home():
-    uid = session.get('id', None)
-    cnt = 0
-    if uid:
-        sql = ["SELECT * FROM post_class"]
-        classLikeList = []
-
-        for i in range(3):
-            cid = session.get(str(i+1), None)
-            if cid != 0:
-                sql.append("SELECT c_name FROM post_class WHERE c_id = {0}".format(cid))
-                classLikeList.append([cid])
-                
-                cnt += 1
-                
-        result = db(True, sql)
-        classList = result[0]
-
-        for i in range(cnt):
-            classLikeList[i].append(result[i+1][0][0])
-            
-        return render_template('home.html', postClass_list = classList , postClass_likeList=classLikeList)
-    else:
-        return redirect('/board/1')
 
 app.run(debug=True)
--- static/javascript/board.js
+++ static/javascript/board.js
@@ -3,7 +3,7 @@ const addLikeClass = (pId, isLiked) => {
         r = ''
         $.ajax({
             type: "POST",
-            url: '/LikeClass/1', //1 => add
+            url: '/likeClass/1', //1 => add
             async: false,
             data: {
                 'classID': pId
@@ -20,7 +20,7 @@ const addLikeClass = (pId, isLiked) => {
         if (r === 's') {
             $('.star').css("font-variation-settings","'FILL' 1,'wght' 400,'GRAD' -25,'opsz' 24")
         } else if (r === 'f') {
-            $('#like').css({"width":"150px"}).val('3개 까지만 가능합니다.')
+            $('#class_like').css({"width":"150px"}).val('3개 까지만 가능합니다.')
             //3개까지만 추가 가능 맨트
         } else {
             $('#asdf').val(r)
