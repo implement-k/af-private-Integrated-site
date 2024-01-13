8344b52d222b6c7be0bf91faa60dcbb02e38c336
koohyun kwon <rnjsrn98@gmail.com>
2024-01-13 14:44:06 +0000

    eb test 4

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,30 +1,36 @@
 from flask import Flask
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+@application.route('/',methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        con = pymysql.connect(user=U,passwd=P,host=H,db=D,charset='utf8')
+        cur = con.cursor()
+        user_id = request.form.get('id')
+        user_pw = request.form.get('password')
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+        if len(user_id) == 0 or len(user_pw) == 0: return redirect('/login',isCorrect=0)
+        
+        # cur.execute("SELECT u_id FROM user WHERE u_id = '{0}' AND u_pw = '{1}';".format(user_id, user_pw))
+        isUser = len(cur.fetchall())
+
+        if isUser > 0:
+            session['id'] = user_id
+
+            # cur.execute("SELECT u_intro FROM user WHERE u_id = '{0}'".format(user_id))
+            session['user_intro'] = cur.fetchall()[0][0]
+
+            con.close()
+            return redirect('/')
+        else:
+            con.close()
+            return render_template('login.html',isCorrect=0)
+    else:
+        if "id" in session: return redirect('/')
+        else: return render_template('login.html',isCorrect=1)
 
 # run the app.
 if __name__ == "__main__":
